150472d0c995e077610557304641b9e701484600
DianaZmeeva <zmeevadiana@mail.ru>
2019-11-28 23:20:56 +0700

    done lab

---
+++ sims_10/sims_10/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace sims_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.weather = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.day = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.weather);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.day);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(284, 261);
+            this.panel1.TabIndex = 0;
+            // 
+            // weather
+            // 
+            this.weather.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.weather.ForeColor = System.Drawing.Color.DarkBlue;
+            this.weather.Location = new System.Drawing.Point(0, 105);
+            this.weather.Name = "weather";
+            this.weather.Size = new System.Drawing.Size(284, 37);
+            this.weather.TabIndex = 2;
+            this.weather.Text = "Clear";
+            this.weather.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // day
+            // 
+            this.day.AutoSize = true;
+            this.day.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.day.Location = new System.Drawing.Point(112, 22);
+            this.day.Name = "day";
+            this.day.Size = new System.Drawing.Size(52, 16);
+            this.day.TabIndex = 0;
+            this.day.Text = "Time: 0";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(0, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(284, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Predict weather through: ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label day;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label weather;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ sims_10/sims_10/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sims_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+        int i, j, index=0;
+        double date = 0, now = 0, final_time, alpha;
+        double[][] q =
+        {
+            new double[] { -0.4, 0.3, 0.1 },
+            new double[] { 0.4, -0.8, 0.4 },
+            new double[] { 0.1, 0.4, -0.5 },
+        };
+
+        double[,] p = new double[3,3];
+        //double[][] p = new double [3][3];
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(now!=final_time)
+            {
+                date++;
+                now = date / 10;
+                day.Text = "Time: " + now;
+            }
+            else
+            {
+                change_text();
+                start();
+            }
+           
+        }
+
+        private void change_text()
+        {
+            if (index == 0)
+                weather.Text = "Clear";
+            if (index == 1)
+                weather.Text = "Cloudy";
+            if (index == 2)
+                weather.Text = "Overcast";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ind();
+            start();
+            button1.Visible = false;
+        }
+
+        private void start()
+        {
+            alpha= rnd.NextDouble();
+            date = 0;
+            now = 0;
+            final_time = Math.Round((Math.Log(alpha) / (q[index][index])), 1);
+            label1.Text = "Predict weather through: " + final_time;
+            timer1.Enabled = true;
+            index=pr(alpha);
+        }
+
+        private int pr(double alpha)
+        {
+            if (alpha < p[index,0])
+                i = 0;
+            else if (alpha < p[index, 0] + p[index, 1])
+                i = 1;
+            else
+                i = 2;
+            return i;
+        }
+
+        private void ind()
+        {
+            for (i=0; i<3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    if (i == j)
+                        p[i,j] =0;
+                    else
+                    {
+                        p[i,j] = Math.Round(Math.Abs(q[i][j] / q[i][i]),2);
+                    }
+                }
+            }
+        }
+    }
+}
